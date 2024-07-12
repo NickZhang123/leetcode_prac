18eb80d1a5eae8273bea0644da2297b497b4884b
nick-from_esxi2-linux <qianganan123@163.com>
2024-07-13 01:00:02 +0800

    多文件编译

---
+++ cpp/01_链表.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+//#include "./tools/01_link_list.h"
+#include "01_link_list.h"
+
+using namespace std;
+
+int main() 
+{
+    cout << "hello" << endl;
+    func();
+    return 0;
+}
---
+++ cpp/tools/01_link_list.cpp
@@ -0,0 +1,11 @@
+
+#include<iostream>
+#include "01_link_list.h"
+
+
+
+void func()
+{
+    std::cout << "func"  << std::endl;
+    return;
+}
